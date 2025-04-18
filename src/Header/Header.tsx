1762a7f179600ecae0ef2ea16841ced59f9ad31b
zairz7018 <zairz7018@gmail.com>
2025-04-18 22:26:23 +0100

    Implement responsive navigation menu

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,11 +3,12 @@ import { Avatar, Indicator } from '@mantine/core';
 import { IconAnchor, IconBell, IconSettings } from "@tabler/icons-react";
 import NavLinks from './NavLinks';
 import { useLocation } from 'react-router-dom';
+import ProfileMenu from './ProfileMenu';
 
 const Header = () => {
   const location = useLocation();
   return location.pathname!="/signup" && location.pathname!="/login" ?<div className="w-full bg-mine-shaft-950 px-6 text-white flex justify-between h-28 items-center font-['poppins']">
-      <div className="flex gap-1 items-center text-bright-sun-400" >
+      <div className="flex gap-1 items-center  text-bright-sun-400" >
 
       <IconAnchor stroke={2.5} className="h-8 w-8"/>
       <div className="text-3xl font-semibold">JobHook</div>
@@ -19,12 +20,8 @@ const Header = () => {
       <div className="flex gap-5 items-center">
       
       
-        <div className="flex items-center gap-2">
-          <div>ZAKARYA</div>
-          
-          <Avatar src="avatar.png"   style={{ width: "35px", height: "35px" }} />
-          
-        </div>
+        
+        <ProfileMenu />
         <div className="bg-mine-shaft-900 p-1.5 rounded-full">
         <IconSettings />
         </div>
---
+++ src/Header/ProfileMenu.tsx
@@ -0,0 +1,76 @@
+import { Menu, Button, Text, Avatar, Switch, rem } from '@mantine/core';
+import {
+  IconSettings,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+  IconUserCircle,
+  IconFileText,
+  IconMoon,
+  IconSun,
+  IconMoonStars,
+  IconLogout2,
+} from '@tabler/icons-react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const ProfileMenu = () => {
+  const [checked, setChecked] = useState(false);
+  const [opened, setOpened] = useState(false);
+  return (
+    <Menu shadow="md" width={200} opened={opened} onChange={setOpened}>
+      <Menu.Target>
+        <div className="flex items-center cursor-pointer gap-2">
+                  <div>ZAKARYA</div>
+                  
+                  <Avatar src="avatar.png"   style={{ width: "35px", height: "35px" }} />
+                  
+                </div>
+      </Menu.Target>
+
+      <Menu.Dropdown onChange={()=>setOpened(true)}>
+        <Link to="/profile">
+          <Menu.Item leftSection={<IconUserCircle size={14} />}>
+            Profile
+          </Menu.Item>
+        </Link>
+        
+        <Menu.Item leftSection={<IconMessageCircle size={14} />}>
+          Message
+        </Menu.Item>
+        <Menu.Item leftSection={<IconFileText size={14} />}>
+          Resume
+        </Menu.Item>
+        
+        <Menu.Item
+          leftSection={<IconMoon size={14} />}
+          rightSection={
+            <Switch checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)}
+             size='md' color='dark.4' onLabel={<IconSun style={{width: rem(16), height: rem(16)}} stroke={2.5} 
+            color='yellow'
+            />}  offLabel={<IconMoonStars 
+            style={{width: rem(16), height: rem(16)}} stroke={2.5}
+            color='cyan'
+            />}
+            
+            />
+          }
+        >
+          Dark Mode
+        </Menu.Item>
+
+        <Menu.Divider />
+        <Menu.Item
+          color="red"
+          leftSection={<IconLogout2 size={14} />}
+        >
+          Logout
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+   );
+}
+export default ProfileMenu;
